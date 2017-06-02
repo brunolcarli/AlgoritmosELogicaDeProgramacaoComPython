a56937daa28f89b4f2e46726cd23e130848b96dd
Bruno L. Carli <brunolcarli@gmail.com>
2017-06-02 13:20:48 -0300

    Add files via upload

---
+++ livro/code/capitulo4/exemplo23.py
@@ -0,0 +1,10 @@
+nome = input("Insira seu nome: ") #primeiro recebemos o nome
+
+"""queremos mostrar o nome 10 vezes
+ao inves de escrever dez vezes print(nome)
+podemos simplesmente fazer o seguinte:
+"""
+
+for contagem in range(10):
+    print(nome)
+
---
+++ livro/code/capitulo4/exemplo24.py
@@ -0,0 +1,3 @@
+
+for i in range(11): #a variavel i e nosso contador
+    print("2 x ", i, " = ", 2 * i) #perceba como o i ira mudar em cada repeticao
